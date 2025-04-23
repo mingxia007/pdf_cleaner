8622a25892aa700535ccc09f0a19c1b8ccb1227f
mingxia007 <mingxia.lin@tum.de>
2025-04-23 12:47:36 +0200

    Rewrite comments

--- pdf_edit.py
+++ pdf_edit.py
@@ -6,20 +6,21 @@ that is irrelevant to the content.The names are also always mixed up. I wrote
 this script to solve the problem."""
 
 def remove_first_page(source_dir):
-    """Remove the first page of all pdf files in the directory and rename them 
-    to a numbered series. 
+    """Remove the first page of all pdf files in the directory, rename the 
+    edited files to a numbered series and stored them in a new diretory.
     """
-    id = 0
+    #Make a new diretory with the same name plus'_edit'
+    #The new directory is in the same parent diretory as the old diretory
     folder_name = os.path.basename(source_dir)
     parent_dir = os.path.dirname(source_dir)
     target_dir = os.path.join(parent_dir, f'{folder_name}_edit')
     os.makedirs(target_dir, exist_ok=True)
 
-    for filename in os.listdir(source_dir):
-
+    for id, filename in enumerate(os.listdir(source_dir)):
         input_pdf_path = os.path.join(source_dir, filename)
         output_pdf_path = os.path.join(target_dir,f"{id}.pdf" )
 
+        #Skipping the none-pdf files
         if not filename.lower().endswith('.pdf'):
             print(f"Skipping non-PDF file: {filename}")
             if filename =='.DS_Store':
@@ -27,20 +28,20 @@ def remove_first_page(source_dir):
             continue
 
         with open(input_pdf_path, 'rb') as input_file:
-            #with neednt to open and close file
-            reader = pr(input_file) #Object
-            writer = pw()#Object
+            #With 'with' neednt to open and close file
+            reader = pr(input_file) 
+            writer = pw()
 
-            #add all of pages except the first
+            #Add all of pages except the first to the new file
             for i in range(1, len(reader.pages)):
                 writer.add_page(reader.pages[i])
-            #name the new file    
             with open(output_pdf_path, 'wb') as output_file:
                 writer.write(output_file) 
 
+        #delete the old file
         os.remove(input_pdf_path)
-        id += 1
 
+    #delete old diretory
     try:
         os.rmdir(source_dir)
     except OSError:
